527faf01f79b4caea3351590295f702f53649834
rattiya2018 <rungkotchasit@gmail.com>
2018-12-15 17:24:07 +0700

    Lab 3:Quiz_BMT

---
+++ src/Lab3/ex1.java
@@ -0,0 +1,28 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is your nickname? : ");
+
+        name = sc.nextLine();
+
+        System.out.println("Hello,"+name);
+//age
+        System.out.print("อายุเท่าไร? : ");
+        int age = sc.nextInt();
+
+        System.out.println("ฉันอายุ "+age+ " ปี ");
+        if(age>30)
+            System.out .println("คุณแก่");
+        else
+            System.out.println("คุณอายุน้อย");
+
+
+    }//main
+}//class
---
+++ src/Lab3/ex2.java
@@ -0,0 +1,38 @@
+package Lab3;
+
+
+import java.util.Scanner;
+public class ex2 {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner sc = new Scanner(System.in);
+        Scanner tt = new Scanner(System.in);
+
+        System.out.print("What is your nickname? : ");
+
+        name = sc.nextLine();
+
+        System.out.println("Hello,"+name);
+//age
+        System.out.print("อายุเท่าไร? : ");
+        int age = tt.nextInt();
+        System.out.println("ฉันอายุ "+age+ " ปี ");
+
+//เพศ
+        System.out.print("คุณเพศอะไร? : ");
+        String a = sc.nextLine();
+        System.out.println("ฉันเพศ "+a+ "  ");
+
+        //ที่อยู่
+        System.out.print("คุณอยู่ที่ไหน? : ");
+        String add = sc.nextLine();
+        System.out.println("ฉันอยู่ที่ "+add+ "  ");
+
+        //  เบอร์โทร
+        System.out.print("คุณเบอร์โทรอะไร? : ");
+        String phen = sc.nextLine();
+        System.out.println("เบอร์โทร. "+phen+ "  ");
+
+    }//main
+}//class
