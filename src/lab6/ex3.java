1231280100cf64f96f8758040719688d4173feb5
rattiya2018 <rungkotchasit@gmail.com>
2018-12-24 15:31:43 +0700

    Lab6:

--- src/lab6/ex3.java
+++ src/lab6/ex3.java
@@ -1,5 +1,28 @@
 package lab6;
 
-public class ex3 {
+import java.util.StringTokenizer;
 
+public class ex3 {
+    public static void main(String[] args) {
+        String myMsg = "I am rung";
+        StringBuffer strBuf = new StringBuffer(myMsg);
+
+        System.out.println(strBuf);//พิมพ์คำที่โค้ดไว้
+        System.out.println(strBuf.reverse());//สลับคำจากหลังไปหน้า
+
+        strBuf.append("Hello");
+        System.out.println(strBuf);//ใช้ในการสลับตัวอักษร
+
+        //
+        StringTokenizer token = new StringTokenizer(strBuf.toString());
+
+        System.out.println(token);
+        System.out.println(token.countTokens());//นับจำนวนคำโดยดูจากการเคาะ
+
+        while (token.hasMoreElements()){
+            System.out.println(token.nextToken());//แยกคำแต่ละบรรทัด
+        }
+
+
+    }
 }
